71a87d572fbb989ce5b7b4dca419c42a18537480
m-yamamo0417 <m.yamamo0417@gmail.com>
2016-07-15 18:58:25 +0900

    add insert and selectAll to MessageMapper.

--- wicketGuiceMyBatis/src/main/java/myamamo0417/mapper/MessageMapper.java
+++ wicketGuiceMyBatis/src/main/java/myamamo0417/mapper/MessageMapper.java
@@ -1,8 +1,16 @@
 package myamamo0417.mapper;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
 public interface MessageMapper {
 
     void create();
 
     void drop();
+
+    void insert(@Param("message") String message);
+
+    List<String> selectAll();
 }
